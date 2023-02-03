f4c12f39b03d171651b201562be06143ce5992f4
tohinsarker55 <tohinsarker55@gmail.com>
2023-02-03 21:59:44 +0600

    61-6

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
+
 
 app.get('')
 app.get('/', (req, res) => {
@@ -12,6 +14,24 @@ app.get('/', (req, res) => {
 });
 app.get('/news-categories', (req,res) => {
     res.send(categories);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if(id === '08'){
+        res.send(news)
+    }else{
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews)
+    }
+});
+app.get('/news', (req, res) => {
+    res.send(news);
+});
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNew = news.find(n => n._id === id);
+    res.send(selectedNew)
 })
 app.listen(port, () => {
     console.log('news running');
